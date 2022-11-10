fc52b5d1de76e918c207e151f30976d7a8bc0ffe
kbc18a11 <wada121400@gmail.com>
2022-11-10 23:26:50 +0900

    名前追加のフォーム作ったよ

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,47 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-
-const names = [
-    '孫',
-    'じーさん',
-    'げべ',
-    '校長',
-    '最強さん',
-    'ちゃむらい'
-]
+import { FormEvent, useState } from 'react';
 
 
-const App = () => {
+const App = (): JSX.Element => {
+    const [name, setName] = useState("");
+
+    const [names, setNames] = useState([
+        '孫',
+        'じーさん',
+        'げべ',
+        '校長',
+        '最強さん',
+        'ちゃむらい'
+    ])
+
+    /**
+     * 名前一覧の表示タグの生成
+     * 
+     * @returns {JSX.Element[]} 
+     */
+    const generateDisplayedName = (): JSX.Element[] => {
+        return names.map((name, nameIndex) => <div id={String(nameIndex)}>{name}</div>);
+    }
+
+    const handleChangName = (event: React.ChangeEvent<HTMLInputElement>) => {
+        setName(event.target.value);
+    };
+
+    const handleSubmit = (event: FormEvent<HTMLFormElement>) => {
+        event.preventDefault();
+
+        setNames([...names, name])
+    }
+
     return (
         <div className="App">
-            <header className="App-header">
-                <img src={logo} className="App-logo" alt="logo" />
-                <p>
-                    Edit <code>src/App.tsx</code> and save to reload.
-                </p>
-                <a
-                    className="App-link"
-                    href="https://reactjs.org"
-                    target="_blank"
-                    rel="noopener noreferrer"
-                >
-                    Learn React
-                </a>
-            </header>
+            <h1>名前一覧</h1>
+            <form onSubmit={handleSubmit}>
+                <input type="text" value={name} name="name" onChange={handleChangName} />
+                <button>名前の追加</button>
+            </form>
+            <div className='names'>
+                {generateDisplayedName()}
+            </div>
         </div>
     );
 }
