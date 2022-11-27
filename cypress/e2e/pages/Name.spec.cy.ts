9062a5b15de5ee780a803b84b11baa6faa8f92ed
kbc18a11 <wada121400@gmail.com>
2022-11-27 14:37:42 +0900

    終わり❤

--- cypress/e2e/pages/Name.spec.cy.ts
+++ cypress/e2e/pages/Name.spec.cy.ts
@@ -2,10 +2,22 @@ describe('トップページ', () => {
     beforeEach(() => {
         cy.visit('http://localhost:3000/');
     });
-    it('じーさんの表示', () => {
+    it('名前一覧に「じーさん」が表示されているか', () => {
+        // #name-0のテキスト属性は、「じーさん」であるか？
         cy.get('#name-0').should('have.text', 'じーさん');
     });
-    it('曽山の入力', () => {
+    it('フォームに「曽山」と入力して、「曽山」が名前一覧に表示されているか', () => {
+        // <input type="text" name="name" /> に「曽山」と入力
         cy.get('[name="name"]').type('曽山');
+        // submitボタンを押す
+        cy.get('form').submit();
+        cy.get('#name-6').should('have.text', '曽山');
+    });
+    it('「じーさん」のページに遷移する', () => {
+        // #name-0に対して、直接クリックしてもaタグをクリックした事にならず、画面遷移はしない。
+        cy.get('#name-0').children('a').click();
+        // URLの日本語は、エンコードされた文字列じゃないと、trueにならない。
+        cy.url().should('include', '/name/%E3%81%98%E3%83%BC%E3%81%95%E3%82%93');
+        cy.get('.name').should('have.text', 'じーさん');
     });
 });
